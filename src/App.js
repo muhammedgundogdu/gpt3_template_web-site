198923fd868066c673c0823cc9f35f3441463f47
muhammedgundogdu <muhammedgundogdu45@gmail.com>
2023-01-03 14:41:18 +0300

    first commit
    first commit the project

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import './App.css'
+
+//27.04 de kaldım
+
+function App() {
+  
+    return (
+      <div className="App" >
+        <div className="gradient__bg">
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+      </div>
+    )
+  }
+  
+
+
+export default App;
